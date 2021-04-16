82b47ddda3f419cbffdccbb3e2c4fd386e1d7b9a
Haily <lyquochai835@gmail.com>
2021-04-16 11:24:39 +0700

    firstcommit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product hhhhhNot Found' });
+  }
+});
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
