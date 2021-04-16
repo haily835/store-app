2b35a978f2872a29374133e3d34af8eeaeb64101
Drakies <58969005+Drakies3172@users.noreply.github.com>
2021-04-16 11:54:30 +0700

    fix homescreen bug

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.get('/api/products', (req, res) => {
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,9 +2,6 @@ const {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
-  PRODUCT_DETAILS_REQUEST,
-  PRODUCT_DETAILS_SUCCESS,
-  PRODUCT_DETAILS_FAIL,
 } = require('../constants/productConstants');
 
 export const productListReducer = (
@@ -17,22 +14,6 @@ export const productListReducer = (
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
-      return { loading: false, error: action.payload };
-    default:
-      return state;
-  }
-};
-
-export const productDetailsReducer = (
-  state = { product: {}, loading: true },
-  action
-) => {
-  switch (action.type) {
-    case PRODUCT_DETAILS_REQUEST:
-      return { loading: true };
-    case PRODUCT_DETAILS_SUCCESS:
-      return { loading: false, product: action.payload };
-    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
